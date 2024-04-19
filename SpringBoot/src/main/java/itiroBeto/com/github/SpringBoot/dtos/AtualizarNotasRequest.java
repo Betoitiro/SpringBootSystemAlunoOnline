3ff8d8d304e6126c4b3f8bb5c9f409dcf19a24de
Humberto Itiró Morimitsu Filho <126424817+Betoitiro@users.noreply.github.com>
2024-04-18 22:04:13 -0300

    AVG alunoOnlie

--- SpringBoot/src/main/java/itiroBeto/com/github/SpringBoot/dtos/AtualizarNotasRequest.java
+++ SpringBoot/src/main/java/itiroBeto/com/github/SpringBoot/dtos/AtualizarNotasRequest.java
@@ -1,6 +1,25 @@
 package itiroBeto.com.github.SpringBoot.dtos;
 
+
+
+//Lombok data n funciona
 public class AtualizarNotasRequest {
     private Double nota1;
     private Double nota2;
+
+    public Double getNota1() {
+        return nota1;
+    }
+
+    public void setNota1(Double nota1) {
+        this.nota1 = nota1;
+    }
+
+    public Double getNota2() {
+        return nota2;
+    }
+
+    public void setNota2(Double nota2) {
+        this.nota2 = nota2;
+    }
 }
--- SpringBoot/src/main/java/itiroBeto/com/github/SpringBoot/service/MatriculaAlunoService.java
+++ SpringBoot/src/main/java/itiroBeto/com/github/SpringBoot/service/MatriculaAlunoService.java
@@ -1,16 +1,21 @@
 package itiroBeto.com.github.SpringBoot.service;
 
+import itiroBeto.com.github.SpringBoot.dtos.AtualizarNotasRequest;
 import itiroBeto.com.github.SpringBoot.enums.MatriculaAlunoStatusEnum;
 import itiroBeto.com.github.SpringBoot.model.MatriculaAluno;
 import itiroBeto.com.github.SpringBoot.repository.MatriculaAlunoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 import java.util.Optional;
 
 @Service
 public class MatriculaAlunoService {
+
+    public static final double GRADE_AVG_TO_APPROVE = 7.0;
 
     @Autowired
     MatriculaAlunoRepository matriculaAlunoRepository;
@@ -31,6 +36,42 @@ public class MatriculaAlunoService {
         return matriculaAlunoRepository.findById(id).orElse(null);
     }
 
-    public
+    public void updateGrade(Long matriculaAlunoId, AtualizarNotasRequest atualizarNotasRequest){
+        MatriculaAluno matriculaAluno =
+                matriculaAlunoRepository.findById(matriculaAlunoId).orElseThrow(
+                        ()-> new ResponseStatusException(HttpStatus
+                                .NOT_FOUND, "Matricula não encontrada!"));
+
+
+
+    }
+
+    public void updateStudentGrades(MatriculaAluno matriculaAluno, AtualizarNotasRequest atualizarNotasRequest){
+        if (atualizarNotasRequest.getNota1() != null){
+            matriculaAluno.setNota1(atualizarNotasRequest.getNota1());
+        }
+        if (atualizarNotasRequest.getNota2()!= null) {
+            matriculaAluno.setNota2(atualizarNotasRequest.getNota2());
+        }
+    }
+
+    public void updateStudentStatus(MatriculaAluno matriculaAluno){
+        Double nota1 = matriculaAluno.getNota1();
+        Double nota2 = matriculaAluno.getNota2();
+
+
+        if (nota1 != null && nota2 != null){
+            double average = (nota1 + nota2) / 2;
+
+
+            //if como operador ternario
+            // ? -> if
+            // : -> else
+            matriculaAluno.setStatus(average >= GRADE_AVG_TO_APPROVE ?
+                    MatriculaAlunoStatusEnum.APROVADO
+                    : MatriculaAlunoStatusEnum.REPROVADO);
+        }
+
+    }
 
 }
